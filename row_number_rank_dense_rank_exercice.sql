1000b81d0acc33c0e6f5a460a803cc93a6fa937f
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-03-31 21:28:00 +0200

    row_number exercice

--- row_number_rank_dense_rank_exercice.sql
+++ row_number_rank_dense_rank_exercice.sql
@@ -1,5 +1,16 @@
 
 
 
-SELECT *
-FROM Customer_Snapshot AS cs
+
+WITH cus_ranking AS 
+(
+	SELECT cs.*,
+	RANK () OVER (ORDER BY cs.FromDate) AS ranking
+	FROM Customer_Snapshot AS cs
+	WHERE cs.Manager = 'Jeremy' AND Club = 'Gold'
+		AND cs.AccountType = 'Business'
+)
+
+SELECT * FROM cus_ranking
+where ranking = 1
+
