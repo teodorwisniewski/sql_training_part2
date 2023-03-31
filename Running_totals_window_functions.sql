1e51ee42960cd87ecf8cefd99a35e96327104495
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-03-31 22:11:29 +0200

    Running_totals_window_functions

--- Running_totals_window_functions.sql
+++ Running_totals_window_functions.sql
@@ -13,6 +13,39 @@ ORDER BY vot.SalesOrderID
 
 
 SELECT *,
-	SUM(vot.LineTotal) OVER (PARTITION BY SalesOrderID ORDER BY vot.OrderDate) AS Running_total
+	SUM(vot.LineTotal) OVER (PARTITION BY vot.SalesOrderID ORDER BY vot.SalesOrderDetailID) AS Running_total
 FROM V_Orders_Test vot
 ORDER BY vot.SalesOrderID
+
+
+
+WITH order_running_totals AS (
+	SELECT *,
+		SUM(vot.LineTotal) OVER (ORDER BY vot.SalesOrderID) AS Running_total,
+		DENSE_RANK() OVER (ORDER BY vot.SalesOrderID) AS order_count
+	FROM V_Orders_Test vot
+)
+
+SELECT TOP 1 *
+FROM order_running_totals
+WHERE Running_total >= 300000
+ORDER BY SalesOrderID
+
+
+
+
+
+WITH order_running_totals AS (
+	SELECT vot.SalesOrderID, SUM(vot.LineTotal) AS TotalOrderValue
+	FROM V_Orders_Test vot
+	GROUP BY vot.SalesOrderID
+)
+
+SELECT  SalesOrderID,
+		TotalOrderValue,
+		SUM(TotalOrderValue) OVER(ORDER BY SalesOrderID) AS tolling_sum, 
+		ROW_NUMBER() OVER(ORDER BY SalesOrderID) AS ranking
+FROM order_running_totals
+ORDER BY SalesOrderID
+
+
