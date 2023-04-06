60d84c6df3126e235f1d4d8a129255fe60fa6db8
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-04-06 10:34:34 +0200

    rank over

--- 20230403/max_experience_join_on_many_columns.sql
+++ 20230403/max_experience_join_on_many_columns.sql
@@ -1,16 +1,30 @@
-WITH max_experience AS (
+-- WITH max_experience AS (
 
-  SELECT project_id,
+--   SELECT project_id,
 
-    MAX(experience_years) max_experience
+--     MAX(experience_years) max_experience
 
-  FROM Project p
+--   FROM Project p
 
-  INNER JOIN Employee USING(employee_id)
+--   INNER JOIN Employee USING(employee_id)
 
-  GROUP BY project_id  
+--   GROUP BY project_id  
 
-)
+-- )
+
+
+
+-- --   SELECT p.project_id, p.employee_id 
+
+-- --   FROM Project p
+
+-- --   INNER JOIN Employee e USING(employee_id)
+
+-- --   INNER JOIN max_experience me ON
+
+-- --   p.project_id = me.project_id AND e.experience_years = me.max_experience
+
+
 
 
 
@@ -20,20 +34,19 @@ WITH max_experience AS (
 
 --   INNER JOIN Employee e USING(employee_id)
 
---   INNER JOIN max_experience me ON
+--   WHERE (p.project_id, e.experience_years) IN 
 
---   p.project_id = me.project_id AND e.experience_years = me.max_experience
-
-
+--   (SELECT * FROM max_experience)
 
 
 
-  SELECT p.project_id, p.employee_id 
 
+  SELECT project_id, employee_id
+  FROM (
+        SELECT p.project_id, p.employee_id,
+        RANK () OVER(PARTITION BY p.project_id ORDER BY e.experience_years DESC) AS rk
   FROM Project p
-
   INNER JOIN Employee e USING(employee_id)
+  ) t
+  WHERE rk =1
 
-  WHERE (p.project_id, e.experience_years) IN 
-
-  (SELECT * FROM max_experience)
