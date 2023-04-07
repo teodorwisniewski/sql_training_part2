6e9529e044b265dddb1b42c03e2f1c6619a7de6f
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-04-07 09:40:13 +0200

    easier solution

--- leetchode/cusomer_placing_orders.sql
+++ leetchode/cusomer_placing_orders.sql
@@ -1,32 +1,43 @@
 
-WITH max_counts AS (
-  SELECT MAX(t.orders_count) max_counts
-  FROM (
-    SELECT customer_number, 
-        COUNT(*) as orders_count
-    FROM Orders
-    GROUP BY customer_number
-  ) t
-), order_counts AS (
-  SELECT customer_number, 
-        COUNT(*) as orders_count
-  FROM Orders
-  GROUP BY customer_number
-)
+-- WITH max_counts AS (
+--   SELECT MAX(t.orders_count) max_counts
+--   FROM (
+--     SELECT customer_number, 
+--         COUNT(*) as orders_count
+--     FROM Orders
+--     GROUP BY customer_number
+--   ) t
+-- ), order_counts AS (
+--   SELECT customer_number, 
+--         COUNT(*) as orders_count
+--   FROM Orders
+--   GROUP BY customer_number
+-- )
 
 
-# SELECT *
-# FROM max_counts
+-- # SELECT *
+-- # FROM max_counts
 
-# SELECT customer_number, 
-#         COUNT(*) as orders_count
-#     FROM Orders
-#     GROUP BY customer_number
+-- # SELECT customer_number, 
+-- #         COUNT(*) as orders_count
+-- #     FROM Orders
+-- #     GROUP BY customer_number
+
+
+-- SELECT customer_number
+-- FROM order_counts
+-- WHERE orders_count IN (
+--   SELECT max_counts
+--   FROM max_counts
+-- )
 
 
 SELECT customer_number
-FROM order_counts
-WHERE orders_count IN (
-  SELECT max_counts
-  FROM max_counts
-)
+
+FROM Orders
+
+GROUP BY customer_number 
+
+ORDER BY COUNT(*) DESC 
+
+LIMIT 1;
