a7b51651c679311e9ea0fe8f7806eb99cca445a8
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-05-16 11:04:04 +0200

    AVG where

--- leetchode/immediate_orders.sql
+++ leetchode/immediate_orders.sql
@@ -1,45 +1,61 @@
-WITH delivery_table AS (
-
-  SELECT delivery_id, customer_id, order_date,
-
-        CASE 
-
-        WHEN order_date=customer_pref_delivery_date THEN 'immediate'
-
-        ELSE 'scheduled' END AS order_type
-
-  FROM Delivery
 
 
 
-), first_orders AS (
-
-  SELECT customer_id, MIN(order_date) AS first_order_date
-
-  FROM delivery_table
-
-  GROUP BY customer_id
-
-), first_order_with_order_type AS (
-
-  SELECT dt.*
-
-  FROM delivery_table dt
-
-  JOIN first_orders fo
-
-  ON dt.customer_id = fo.customer_id AND fo.first_order_date = dt.order_date
-
+SELECT ROUND(
+    100 * AVG(order_date=customer_pref_delivery_date )
+    , 2
+) AS immediate_percentage  
+FROM Delivery 
+WHERE (customer_id, order_date)
+IN (SELECT customer_id, MIN(order_date)
+    FROM Delivery 
+    GROUP BY  customer_id
 )
 
 
 
+-- WITH delivery_table AS (
+
+--   SELECT delivery_id, customer_id, order_date,
+
+--         CASE 
+
+--         WHEN order_date=customer_pref_delivery_date THEN 'immediate'
+
+--         ELSE 'scheduled' END AS order_type
+
+--   FROM Delivery
+
+
+
+-- ), first_orders AS (
+
+--   SELECT customer_id, MIN(order_date) AS first_order_date
+
+--   FROM delivery_table
+
+--   GROUP BY customer_id
+
+-- ), first_order_with_order_type AS (
+
+--   SELECT dt.*
+
+--   FROM delivery_table dt
+
+--   JOIN first_orders fo
+
+--   ON dt.customer_id = fo.customer_id AND fo.first_order_date = dt.order_date
+
+-- )
 
 
 
 
-SELECT ROUND(100.0*(SELECT COUNT(*) FROM first_order_with_order_type
 
-WHERE order_type='immediate')/COUNT(*),2) AS immediate_percentage 
 
-FROM first_order_with_order_type dt
+
+-- SELECT ROUND(100.0*(SELECT COUNT(*) FROM first_order_with_order_type
+
+-- WHERE order_type='immediate')/COUNT(*),2) AS immediate_percentage 
+
+-- FROM first_order_with_order_type dt
