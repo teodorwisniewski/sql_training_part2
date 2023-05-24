d5d494944b87b4ea5694ee69c47875b37350e499
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-05-24 22:42:26 +0200

    self join

--- leetchode/calls_duration_min.sql
+++ leetchode/calls_duration_min.sql
@@ -1,19 +1,25 @@
 
-WITH without_duplicates AS (
-    SELECT DISTINCT *
-    FROM Calls
-), making_pairs AS (
-    SELECT LEAST(from_id, to_id) AS person1,
-           GREATEST(from_id, to_id) AS person2,
-           duration
-    FROM Calls
-)
+-- WITH without_duplicates AS (
+--     SELECT DISTINCT *
+--     FROM Calls
+-- ), making_pairs AS (
+--     SELECT LEAST(from_id, to_id) AS person1,
+--            GREATEST(from_id, to_id) AS person2,
+--            duration
+--     FROM Calls
+-- )
 
-# SELECT *
-# FROM making_pairs
+-- # SELECT *
+-- # FROM making_pairs
 
-SELECT person1, person2,
-        COUNT(duration) AS call_count,
-        SUM(duration) AS total_duration 
-FROM making_pairs
-GROUP BY person1, person2
+-- SELECT person1, person2,
+--         COUNT(duration) AS call_count,
+--         SUM(duration) AS total_duration 
+-- FROM making_pairs
+-- GROUP BY person1, person2
+
+select mgr.employee_id, mgr.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age
+from employees emp join employees mgr
+on emp.reports_to = mgr.employee_id
+group by employee_id
+order by employee_id
