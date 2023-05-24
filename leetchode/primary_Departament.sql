4eddeedc1862a8e751bb642d8f4915abff095a5c
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-05-24 22:54:18 +0200

    union

---
+++ leetchode/primary_Departament.sql
@@ -0,0 +1,26 @@
+WITH counted_departaments AS (
+  SELECT employee_id, COUNT(department_id) dept_count
+  FROM Employee 
+  GROUP BY employee_id 
+), only_one_dept AS (
+  SELECT e.employee_id, e.department_id 
+  FROM (SELECT * FROM counted_departaments WHERE dept_count=1) ond
+  JOIN Employee e
+  ON ond.employee_id = e.employee_id 
+), multiple_dept AS (
+  SELECT e.employee_id, e.department_id 
+  FROM (SELECT * FROM counted_departaments WHERE dept_count>1) ond
+  JOIN Employee e
+  ON ond.employee_id = e.employee_id   
+  WHERE e.primary_flag = 'Y'
+), joined_results AS (
+  SELECT *
+  FROM multiple_dept
+  UNION 
+  SELECT *
+  FROM only_one_dept
+)
+
+SELECT *
+FROM joined_results
+ORDER BY employee_id 
