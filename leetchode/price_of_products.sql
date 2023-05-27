f0f2698cd8ebe385c2e1af828f79b8268fe72d5b
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-05-27 13:19:57 +0200

    union group by where

--- leetchode/price_of_products.sql
+++ leetchode/price_of_products.sql
@@ -22,12 +22,34 @@
 -- RIGHT JOIN unique_products up ON p.product_id=up.product_id
 
 
-SELECT DISTINCT product_id,
-        FIRST_VALUE(new_price) OVER(PARTITION BY product_id ORDER BY change_date DESC) AS price
-FROM Products
-WHERE change_date <= '2019-08-16'
-UNION 
+-- SELECT DISTINCT product_id,
+--         FIRST_VALUE(new_price) OVER(PARTITION BY product_id ORDER BY change_date DESC) AS price
+-- FROM Products
+-- WHERE change_date <= '2019-08-16'
+-- UNION 
+-- SELECT product_id, 10 AS price
+-- FROM Products
+-- WHERE product_id NOT IN (SELECT product_id FROM Products
+-- WHERE change_date <= '2019-08-16' )
+
+
+
+
+
+
 SELECT product_id, 10 AS price
 FROM Products
-WHERE product_id NOT IN (SELECT product_id FROM Products
-WHERE change_date <= '2019-08-16' )
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
+
+UNION
+
+SELECT product_id, new_price AS price
+FROM Products
+WHERE (product_id, change_date)
+IN (
+  SELECT product_id, MAX(change_date)
+  FROM Products
+  WHERE change_date <= '2019-08-16'
+  GROUP BY product_id
+)
